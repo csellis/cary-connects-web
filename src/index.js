a01fb3ab0d375a27d1be2d63074559774ce4c033
Danny Allegrezza <danny@dannyallegrezza.com>
2018-10-18 08:21:02 -0400

    Continue cleanup
    * Remove unused files
    * Reference singular styles.css for homepage/theme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './utilities/registerServiceWorker';
 import App from './App';
 
-import './components/Map/styles.css';
+import './styles/styles.css';
 import 'leaflet/dist/leaflet.css';
 
 render(
